0abc3cd425ea1d7927c9ccef949de8d40ed7eb98
Hashem <116127422+HASHEM2001@users.noreply.github.com>
2023-01-25 00:16:44 +0200

    Update sudoku.py

--- sudoku.py
+++ sudoku.py
@@ -1,91 +1,106 @@
-from pprint import pprint
+import pyautogui
+import time
+
+grid=[]
+while True:
+    X= list(input("Row:"))
+    ints=[]
+    for n in X :
+        ints.append(int(n))
+    grid.append(ints)
+    if len(grid)==9:
+        break
+    print("ROw :"+str(len(grid))+" complit")
+
+
+time.sleep(3)
+
 
 
 def find_next_empty(puzzle):
-    # finds the next row, col on the puzzle that's not filled yet --> rep with -1
-    # return row, col tuple (or (None, None) if there is none)
-
-    # keep in mind that we are using 0-8 for our indices
+    #find the next row , col on the puzzle that's not filled yet --> 0
+    #return row ,col tuple (or (none,none)if there is none)
     for r in range(9):
-        for c in range(9): # range(9) is 0, 1, 2, ... 8
-            if puzzle[r][c] == -1:
+        for c in range(9):
+            if puzzle[r][c]== 0:
                 return r, c
+    return None, None # if no spaces in the puzzle are empty(0)
 
-    return None, None  # if no spaces in the puzzle are empty (-1)
-
-def is_valid(puzzle, guess, row, col):
-    # figures out whether the guess at the row/col of the puzzle is a valid guess
-    # returns True or False
-
-    # for a guess to be valid, then we need to follow the sudoku rules
-    # that number must not be repeated in the row, column, or 3x3 square that it appears in
-
-    # let's start with the row
-    row_vals = puzzle[row]
+def is_valif(puzzle,guess,row,col):
+    #figuers out whtere the huess at the row,clo of the puzzle is a valid guess
+    #return True if is valid , False otherwise
+    #the row 
+    row_vals=puzzle[row]
     if guess in row_vals:
-        return False # if we've repeated, then our guess is not valid!
-
-    # now the column
-    # col_vals = []
-    # for i in range(9):
-    #     col_vals.append(puzzle[i][col])
-    col_vals = [puzzle[i][col] for i in range(9)]
-    if guess in col_vals:
         return False
-
-    # and then the square
-    row_start = (row // 3) * 3 # 10 // 3 = 3, 5 // 3 = 1, 1 // 3 = 0
-    col_start = (col // 3) * 3
-
-    for r in range(row_start, row_start + 3):
-        for c in range(col_start, col_start + 3):
-            if puzzle[r][c] == guess:
+    # the cloumn
+    # column_vals=[]
+    # for i in range(9):
+    #     column_vals.append(puzzle[i][col])
+    column_vals=[puzzle[i][col] for i in range(9)]
+    if guess in column_vals:
+        return False
+    
+    # the 3*3 Matrixs
+    # we want to get where the 3x3 square starts
+    # and iterate over the 3 values in the row/cloumn
+    row_start=(row//3)*3 #1//3=0 ,5//3=1, 7//3=2
+    col_start=(col//3)*3
+    for r in range(row_start,row_start+3):
+        for c in range(col_start,col_start+3):
+            if guess== puzzle[r][c]:
                 return False
-
+    # if we get here , these checks pass
     return True
 
-def solve_sudoku(puzzle):
-    # solve sudoku using backtracking!
-    # our puzzle is a list of lists, where each inner list is a row in our sudoku puzzle
-    # return whether a solution exists
-    # mutates puzzle to be the solution (if solution exists)
-    
-    # step 1: choose somewhere on the puzzle to make a guess
-    row, col = find_next_empty(puzzle)
 
-    # step 1.1: if there's nowhere left, then we're done because we only allowed valid inputs
-    if row is None:  # this is true if our find_next_empty function returns None, None
-        return True 
+def solve_sudoku(puzzle):
+    #solve sudoku uisig backtracking 
+    #our puzzle is a list of list , where each inner list is a row in our sudoku puzzle 
     
-    # step 2: if there is a place to put a number, then make a guess between 1 and 9
-    for guess in range(1, 10): # range(1, 10) is 1, 2, 3, ... 9
-        # step 3: check if this is a valid guess
-        if is_valid(puzzle, guess, row, col):
-            # step 3.1: if this is a valid guess, then place it at that spot on the puzzle
-            puzzle[row][col] = guess
-            # step 4: then we recursively call our solver!
+    #step 1: choose somewhere on the puzzle to make a guess 
+    row ,col = find_next_empty(puzzle)
+
+    #step 1.1 :if there's nowhere left , then we're done cuz we only allowed valif inputs
+    if row is None:
+        return True
+    
+    #step 2 :if there is a place to put a number , then make a guess between 1 and 9 
+    for guess in range(1,10):
+        #step 3: check if is valid guess
+        if is_valif(puzzle,guess,row,col):
+            #step 3.1: if this is valid then place that guess on the puzzle
+            puzzle[row][col]=guess
+            #Now recurse using this puzzle!
+            #step 4 :recursively call our function
             if solve_sudoku(puzzle):
                 return True
-        
-        # step 5: it not valid or if nothing gets returned true, then we need to backtrack and try a new number
-        puzzle[row][col] = -1
+        #step 5: if not valid or if our guess not solve the puzzel , theen we need to backtrack and try new number 
+        puzzle[row][col]=0
 
-    # step 6: if none of the numbers that we try work, then this puzzle is UNSOLVABLE!!
+    #step 6: if none of the numbers that we
     return False
 
-if __name__ == '__main__':
-    example_board = [
-        [3, 9, -1,   -1, 5, -1,   -1, -1, -1],
-        [-1, -1, -1,   2, -1, -1,   -1, -1, 5],
-        [-1, -1, -1,   7, 1, 9,   -1, 8, -1],
+def printe(matrix):
+    final=[]
+    str_final=[]
+    for i in range(9):
+        final.append(matrix[i])
+    for lists in final:
+        for num in lists:
+            str_final.append(str(num))
+    counter =[]
+    for num in str_final:
+        pyautogui.press(num)
+        pyautogui.hotkey('right')
+        counter.append(num)
+        if len(counter)%9==0:
+            pyautogui.hotkey('down')
+            for i in range(8):
+                pyautogui.hotkey('left')
 
-        [-1, 5, -1,   -1, 6, 8,   -1, -1, -1],
-        [2, -1, 6,   -1, -1, 3,   -1, -1, -1],
-        [-1, -1, -1,   -1, -1, -1,   -1, -1, 4],
 
-        [5, -1, -1,   -1, -1, -1,   -1, -1, -1],
-        [6, 7, -1,   1, -1, 5,   -1, 4, -1],
-        [1, -1, 9,   -1, -1, -1,   2, -1, -1]
-    ]
-    print(solve_sudoku(example_board))
-    pprint(example_board)
+
+print(solve_sudoku(grid))
+printe(grid)
+print(grid)
